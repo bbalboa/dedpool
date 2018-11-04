36eeb265673526da15d53bc5a0305cd2a491f0b1
benjamin <benspak@gmail.com>
2018-11-04 08:16:17 -0500

    Moved to Dedpoll.org && new repo for hosting

--- dev-server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(3000, () => console.log("Server listening on port 3000!"));
